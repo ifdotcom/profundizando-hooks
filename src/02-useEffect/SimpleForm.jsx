155ae375c2210a1728ce06428afdbefadda1f1e3
Fernanda Vidal <ifervb@outlook.com>
2024-06-11 21:14:22 -0600

    Add: useEffect

--- src/02-useEffect/SimpleForm.jsx
+++ src/02-useEffect/SimpleForm.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { useState } from "react";
+import { Message } from "./Message";
 
 export const SimpleForm = () => {
   const [formState, setFormState] = useState({
@@ -9,12 +11,12 @@ export const SimpleForm = () => {
   // se destrucutra el objeto del estado para usarlos en los values de los inputs
   const { username, email } = formState;
 
-// onInputChange recibe el event, pero se  puede desestrucutrar para sacar solo el target {target};
-//  del target solo nos interesa el value y también que elemento es el que está cambiando, entonces: podemos destructurar el name y el value const {name, value} = target;
-  const onInputChange = ({target}) => {
-    const {name, value} = target;
+  // onInputChange recibe el event, pero se  puede desestrucutrar para sacar solo el target {target};
+  //  del target solo nos interesa el value y también que elemento es el que está cambiando, entonces: podemos destructurar el name y el value const {name, value} = target;
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
 
-  // cambio del estado
+    // cambio del estado
     setFormState({
       // se destructura para mantener todos los valores del formulario
       // solo modicar el input (name) que está siendo modificando
@@ -22,9 +24,21 @@ export const SimpleForm = () => {
 
       // name = username : "value"
       // name = email : "value"
-      [name]:value
-    })
+      [name]: value,
+    });
   };
+
+  // El efecto solo se dispara cada que haya un cambio en el formulario
+  useEffect(() => {
+    // console.log("formState changed");
+  }, [formState]);
+
+  // Disparar efecto solo cuando el email cambie
+  useEffect(() => {
+    // console.log("Email changed");
+  }, [email]);
+
+
   return (
     <>
       <h1>Formulario</h1>
@@ -48,6 +62,9 @@ export const SimpleForm = () => {
         value={email}
         onChange={onInputChange}
       />
+      {
+        (username === "ifdotcom2") && <Message/>
+      }
     </>
   );
 };
